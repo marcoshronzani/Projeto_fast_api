2c2f7f41a4e0b516294d1982cbbcae4c74201b28
marcoshronzani <marcoshronzani@gmail.com>
2025-02-17 18:01:25 -0300

    Inicio do Projeto

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from projeto_fast_api.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange ( Preparação )
+
+    response = client.get('/')  # Act ( Ação )
+
+    assert response.status_code == HTTPStatus.OK  # Assert ( Verificação )
+
+    assert response.json() == {'message': 'Olá Mundo!'}
